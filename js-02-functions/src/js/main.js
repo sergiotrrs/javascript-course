396ef1dac7157aa8209be31e529aa04f04b20558
Sergio Torres <sergio.torres@idr.edu.mx>
2025-01-28 11:55:20 -0600

    feat: funciones declaradas

---
+++ js-02-functions/src/js/main.js
@@ -0,0 +1,72 @@
+console.log("JS-02 funciones");
+
+/*
+ Las funciones permiten organizar, reutilizar y 
+ estructurar el código. Su propósito principal es:
+
+- Modularidad: Dividir un programa en piezas lógicas 
+  más pequeñas y manejables.
+- Reutilización de código: Reducir la duplicación de 
+  código al reutilizar funciones.
+- Legibilidad: Hacer que el código sea más claro y fácil de entender.
+- Mantenimiento: Facilitar actualizaciones o modificaciones 
+  al concentrar la lógica en un solo lugar.
+- Abstracción: Ocultar detalles internos y expone 
+  solo lo necesario para trabajar con ellas
+
+  Recomendaciones para el uso de las funciones:
+
+- Nombre descriptivo: Debe indicar claramente lo que hace.
+  Se recomienda comenzar con un verbo
+- Parámetros: Debe tener cero o más parámetros, separados por coma.
+  Se recomienda no tener más de 3 parámetros.
+- Seguir el principo SOLID, sobre el principio de responsabilidad única.
+  (Single Responsibility Principle).
+- Evitar efectos secundarios: Una función no debe modificar variables externas.
+
+*/
+
+// =======================================================================
+
+/*
+https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+----------- Funciones declaradas --------------
+(function declaration, function statement)
+Una característica de las funciones declaradas es que tien hoisting.
+
+sintaxis:
+function nombreFuncionCamelCase ( parámetros ){
+    //cuerpo de la función
+    instrucciones;
+}
+
+hoisting: comportamiento en JS que permite a las declaración de variables(var)
+o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
+
+*/
+
+console.log( saludar("Erick") );
+console.log( saludar("Serch") );
+console.log( saludar("Arely") );
+
+function saludar( nombre){
+    return `Hola ${nombre}, soy una función declarada`;
+}
+// En la función anterior saludar()
+// Agregar el parámetro nombre y mostrar "Hola nombre, soy una función declarada"
+
+
+/*
+ ------------ Funciones expresadas -----------------------
+            (function expressions)
+Son funciones declaradas dentro de la asignación de una variable.
+Estas funciones pueden ser anónimas ( no tienen nombre ).
+Las funciones expresadas no tiene hoisting, porque no se
+carga en memoria hasta que se utilice.
+
+sintaxis:
+    const nombreVariable = function nombreFuncion (parámetros){
+        instrucciones;
+    };
+*/
+
