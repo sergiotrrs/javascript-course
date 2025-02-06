e349eb2ec46f6366e2e68278a75267878d7dee31
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-06 14:27:31 -0600

    feat: userCard() y createUsersCards()

---
+++ js-07-api_fetch/src/modules/cards/generateUsersCards.js
@@ -0,0 +1,37 @@
+import userCard from "./userCard.js"; // importación por default
+
+/**
+ * Crea un grid de tarjetas de usuarios
+ * @param {Object array} userArray Arreglo de usuarios
+ */
+const generateUsersCards = ( userArray )=>{
+
+    // const cards = userArray.map( (element, index , array )=>  );
+    const cards = userArray.map( ( user )=>  userCard( user ) );
+
+    return cards.join("");
+
+}
+
+export {  generateUsersCards };
+
+/*
+ [
+        {
+            "id": 1,
+            "email": "george.bluth@reqres.in",
+            "first_name": "George",
+            "last_name": "Bluth",
+            "avatar": "https://reqres.in/img/faces/1-image.jpg"
+        },
+        {
+            "id": 2,
+            "email": "janet.weaver@reqres.in",
+            "first_name": "Janet",
+            "last_name": "Weaver",
+            "avatar": "https://reqres.in/img/faces/2-image.jpg"
+        },
+ ]
+
+
+*/
