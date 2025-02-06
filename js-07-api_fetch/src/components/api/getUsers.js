5a644e2a8932cc4a4bdac9ed3a1ed48378855430
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-06 13:47:41 -0600

    feat: getUser() a las dos páginas de las API

--- js-07-api_fetch/src/components/api/getUsers.js
+++ js-07-api_fetch/src/components/api/getUsers.js
@@ -4,3 +4,12 @@
  * 
  *  Lista de usuarios: https://reqres.in/api/users?page=2
  */
+const getUsers = (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((users) => console.log(users.data))
+      .catch((error) => console.log(error))
+      .finally(() => console.log("He terminado"));
+  };
+  getUsers("https://reqres.in/api/users?delay=5&page=1"); // Página 1 con retardo de 5 segundos
+  getUsers("https://reqres.in/api/users?page=2"); // Página 2, sin retardo
