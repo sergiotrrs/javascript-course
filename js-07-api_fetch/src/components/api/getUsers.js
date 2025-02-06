d4a9b7f50fdae2f935d2cdfd1d4a0ba2943bf888
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-06 14:36:34 -0600

    feat: tarjetas de usuario

--- js-07-api_fetch/src/components/api/getUsers.js
+++ js-07-api_fetch/src/components/api/getUsers.js
@@ -1,3 +1,5 @@
+import { generateUsersCards } from "../../modules/cards/generateUsersCards.js";
+
 /**
  *  Obtener los usuarios de la siguiente URL e imprimir en consola
  *  https://reqres.in/
@@ -5,11 +7,16 @@
  *  Lista de usuarios: https://reqres.in/api/users?page=2
  */
 const getUsers = (url) => {
+
+    const createCards = ( usersArray )=>{
+        document.getElementById("cards").innerHTML = generateUsersCards( usersArray);
+    }
+
     fetch(url)
       .then((res) => res.json())
-      .then((users) => console.log(users.data))
+      .then((users) => createCards( users.data ) )
       .catch((error) => console.log(error))
       .finally(() => console.log("He terminado"));
   };
-  getUsers("https://reqres.in/api/users?delay=5&page=1"); // Página 1 con retardo de 5 segundos
+  // getUsers("https://reqres.in/api/users?delay=5&page=1"); // Página 1 con retardo de 5 segundos
   getUsers("https://reqres.in/api/users?page=2"); // Página 2, sin retardo
