cc650bb31f7779f8e1133f3dd7e99e0645623666
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-07 14:20:21 -0600

    feat: ejercicio pinkyPromise

---
+++ js-07-api_fetch/src/components/promise/pinkyPromise.js
@@ -0,0 +1,20 @@
+
+
+const pinkyPromise = ( parametro ) =>{
+
+  const myPromise = new Promise( (resolve, reject)=>{
+     // TODO evaluar el parámetro y determinar si se resuelve o rechaza
+  });
+
+  return myPromise;
+}
+
+const secuenciaPrincipal = () =>{
+   
+    console.log("Comenzando Secuencia Principal");
+    // TODO hacer el manejo de la promera pinkyPromise( miArgumento );
+    console.log("Terminando mi secuencia principal")
+
+}
+
+secuenciaPrincipal();
