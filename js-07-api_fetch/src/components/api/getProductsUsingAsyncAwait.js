33f45790af350245cadf98173dcb04fec85e1fd2
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-07 13:04:35 -0600

    feat: manejo de excepciones

--- js-07-api_fetch/src/components/api/getProductsUsingAsyncAwait.js
+++ js-07-api_fetch/src/components/api/getProductsUsingAsyncAwait.js
@@ -1,30 +1,30 @@
-
 /*
 Async y Await facilita la escritura y lectura de código asíncrono.
  Permite escribir código asíncrono de manera similar a cómo se escribiría
  código síncrono.
 */
 
-async function functionName () {
-   await fetch( url );
+async function functionName() {
+  await fetch(url);
 }
 
-const getProductsUsingAsyncAwait = async ( url ) =>{
-    try{
-      console.log("Estoy iniciando en la función con Async y Await");
-      const resolve = await fetch( url );
-      const products = await resolve.json();
-      console.log( products );
-      console.log("Finalizado la petición fetch con Async y Await");
-    } catch( error ){
-      console.warn("No corro, no grito, no empujo");
-      console.error("Que pasó?", error );
-    }
-    
-
-
+const getProductsUsingAsyncAwait = async (url) => {
+  console.log("Estoy iniciando en la función con Async y Await");
+  const resolve = await fetch(url);
+  const products = await resolve.json();
+  console.log(products);
+  console.log("Finalizado la petición fetch con Async y Await");
 };
 
-console.log("Antes de la llamada de la función");
-getProductsUsingAsyncAwait("https://fakestoreapi/products");
-console.log("Después de la llamada de la función");
+const realizaSecuenciaPrincipal = async () => {
+  try {
+    console.log("Antes de la llamada de la función");
+    await getProductsUsingAsyncAwait("https://fakestoreapi/products");
+    console.log("Después de la llamada de la función");
+  } catch (error) {
+    console.warn("No corro, no grito, no empujo");
+    console.error("Que pasó?", error);
+  }
+};
+
+realizaSecuenciaPrincipal();
