4403980680f62242eeea011c343bfcbe900446c0
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-07 11:01:42 -0600

    feat: funcion getUsersUsingAsyncAwait()

---
+++ js-07-api_fetch/src/components/api/getUsersUsingAsyncAwait.js
@@ -0,0 +1,19 @@
+import { generateUsersCards } from "../../modules/cards/generateUsersCards.js";
+
+/**
+ *  Obtener los usuarios de la siguiente URL e imprimir en consola
+ *  https://reqres.in/
+ * 
+ *  Lista de usuarios: https://reqres.in/api/users?page=2
+ */
+const getUsersUsincAsyncAwait = (url) => {
+
+    const createCards = ( usersArray )=>{
+        document.getElementById("cards").innerHTML = generateUsersCards( usersArray);
+    }
+
+
+    
+  };
+  // getUsers("https://reqres.in/api/users?delay=5&page=1"); // Página 1 con retardo de 5 segundos
+  getUsers("https://reqres.in/api/users?page=2"); // Página 2, sin retardo
