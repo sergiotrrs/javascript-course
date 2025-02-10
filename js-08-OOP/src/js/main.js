9d3c1d1da68e20f1f9a438af5d646e48beb6686f
Sergio Torres <sergio.torres@idr.edu.mx>
2025-02-10 14:33:35 -0600

    feat: generación de la clase Product

--- js-08-OOP/src/js/main.js
+++ js-08-OOP/src/js/main.js
@@ -1,6 +1,63 @@
 import { insertMainHeader } from "/src/modules/header/header.js";
-import { insertMainFooter } from "/src/modules/footer/footer.js";
+import { insertMainFooter } from "/src/modules/footer/footer.js"; // ruta absoluta
+// import { amdRadeonRx } from "../modules/class/amd-radeon-rx.js"; // ruta relativa
+import { amdRadeonRx, mouseGamerPixArt } from "/src/modules/class/amd-radeon-rx.js"; // ruta absoluta
+import { Product } from "../modules/class/Product.js";
+
+/*
+ Ruta relativa: Son rutas que dependen de la ubicación actual del archivo. 
+ Rutas absolutas: Especifican la ubicación exacta de un archivo 
+    o recurso sin importar desde dónde se acceda.
+*/
 
 insertMainHeader();
 insertMainFooter();
 
+console.log( amdRadeonRx );
+// Podemos acceder a los atributos usando el operador objeto.atributo (access and call), n.precedence: 17
+// podemos acceder usando objeto[nombreAtributo]
+
+
+// Accediendo al modelo
+console.log( amdRadeonRx.model ); // RX7800XT
+// console.log( amdRadeonRx["model"] ); // RX7800XT
+let attribute = "model"
+console.log( amdRadeonRx[attribute] ); // RX7800XT
+attribute = "price";
+console.log( amdRadeonRx[attribute] ); // 10699
+
+// Acceder al atributo title e imprimir en consola, usando los dos métodos para aceder
+
+console.log( amdRadeonRx["title"]); // Tarjeta de Video Po
+console.log( amdRadeonRx.title); // Tarjeta de Video Po
+let titulo = "title"
+console.log( amdRadeonRx[titulo] ); // Tarjeta de Video Po
+
+const number = 1;
+console.log( amdRadeonRx[`dato${number}`] ); // Tarjeta de Video Po
+
+// accediendo a atributos que no existen
+console.log( amdRadeonRx.dato1 ); // Descrip 1
+console.log( mouseGamerPixArt.dato1 ); //  undefined
+
+console.log( mouseGamerPixArt.compatibilidad ); //  undefined
+
+// Obteniendo el número de elementos
+console.log( amdRadeonRx.compatibilidad.length ); // 2
+//console.log( mouseGamerPixArt.compatibilidad.length ); // Cannot read properties of undefined
+
+// Operador de encadenamiento opcional  ( ?. )
+console.log( mouseGamerPixArt.compatibilidad?.length ); 
+
+console.log( amdRadeonRx.imprimirPrecioMxN() );
+console.log( amdRadeonRx.imprimirPrecioDolares() );
+
+console.log( mouseGamerPixArt.imprimirPrecioMxN() );
+console.log( mouseGamerPixArt.imprimirPrecioDolares() );
+
+//============= Usando la clase Product =====================
+
+const mouseLogi = new Product("Mouse");
+const tecladoGamer = new Product("Teclado");
+const monitorHp = new Product("Monitor");
+const bateria = new Product();
---
+++ js-08-OOP/src/modules/class/Product.js
@@ -0,0 +1,18 @@
+
+/**
+ *  Clase producto
+ *  El nombre de las clases(plantillas) debe ser con UpperCamelCase
+ *  Se recomienda que el nombre sea un sustantivo.
+ * 
+ *
+ * */
+ 
+class Product {
+     // El método constructor nos ayuda a inicializar un objeto
+    constructor( productName="---" ){
+        console.log(`Te he dado vida ${productName}`);
+
+    }
+}
+
+export { Product };
